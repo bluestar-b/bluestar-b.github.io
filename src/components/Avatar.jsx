96f97a274da8c2317302a2c758885392c206f5c1
bluestar_ <canikissfemboy@gmail.com>
2024-01-05 11:15:23 +0700

    reduce avatar size

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,8 +5,9 @@ const Avatar = () => {
     <div className="h-32 w-32 md:h-48 md:w-48">
       <Image
         className="rounded-xl"
-        width={200}
-        height={200}
+        width={64}
+        height={64}
+        quality={80}
         src="/pfp.jpeg"
         alt="Profile picture"
       />
