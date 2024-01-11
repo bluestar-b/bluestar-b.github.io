2f37f549b095b346245baadf7c818223514a7241
bluestar_ <canikissfemboy@gmail.com>
2024-01-12 03:40:46 +0700

    add custom not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <p className="text-md font-bold md:text-2xl">
+        Oops! The page you're looking for is lost in space.
+      </p>
+      <Link
+        href="/"
+        className="text-md p-2 font-bold underline-offset-2 hover:underline"
+      >
+        Back to launch pad
+      </Link>
+    </div>
+  )
+}
