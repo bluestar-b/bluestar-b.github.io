653a6380ebd3d86fb0c40811253f6ff5ad40f299
bluestar_ <canikissfemboy@gmail.com>
2024-01-29 16:27:33 +0700

    fix linter error and add fav music section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Heading from "@/components/Heading"
 import { ExternalLink } from "@/components/ExternalLink"
+import Image from "next/image"
+import FavoriteMusicList from "../components/FavoriteMusicList"
 
 export default function Home() {
   return (
@@ -10,7 +12,8 @@ export default function Home() {
           I joined the world on April 3rd, 2008, and became a space enthusiast,
           studying in a non-formal setting because life is crazy to me 🧨. I
           started learning things in 2020 and began applying them in 2021, and
-          I'm still soaring through the zero gravity of life. How about you?
+          I&apos;m still soaring through the zero gravity of life. How about
+          you?
           <div className="mb-4 mt-1">
             <div className="text-base font-light italic leading-relaxed text-pink-600 dark:text-pink-300">
               &quot;Life is boring, but life still has colors to explore. Find a
@@ -19,6 +22,9 @@ export default function Home() {
             - 2023
           </div>
         </div>
+
+        <h2 className="text-2xl font-semibold mb-2">Favorite song</h2>
+        <FavoriteMusicList />
       </div>
     </main>
   )
---
+++ src/components/FavoriteMusicList.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link"
+
+const FavoriteMusicList = () => {
+  const MusicList = [
+    {
+      title: "Asu no Yozora Shoukaihan",
+      artist: "Orangestar",
+      link: "https://youtu.be/XogSflwXgpw",
+    },
+    {
+      title: "Natsuiro Answer",
+      artist: "Orangestar",
+      link: "https://youtu.be/S6_zybtKN5k",
+    },
+    {
+      title: "Kuusou Ressha",
+      artist: "Orangestar",
+      link: "https://youtu.be/xzoShzMIlIM",
+    },
+    {
+      title: "What I've done",
+      artist: "Linkin Park",
+      link: "https://youtu.be/8sgycukafqQ",
+    },
+  ]
+
+  return (
+    <div className="justify-center">
+      {MusicList.map((music, index) => (
+        <div
+          className="mt-1 min-w-full overflow-hidden overflow-ellipsis"
+          key={index}
+        >
+          <div>
+            <div className="text-md">
+              <span>{music.artist}</span> -{" "}
+              <Link className="hover:underline" href={music.link}>
+                {music.title}
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default FavoriteMusicList
