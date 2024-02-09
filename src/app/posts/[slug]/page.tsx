c9434e0773d529303d930afa2032c29542dc3aa9
bluestar_ <canikissfemboy@gmail.com>
2024-02-09 22:25:28 +0700

    Renovate prose layout

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -15,18 +15,21 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
 
   return (
-    <article className="mx-auto max-w-xl py-8">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
-      </div>
-      <div
-        className="prose p-4 lg:prose-md prose-neutral dark:prose-invert prose-pre:border prose-pre:bg-zinc-200 prose-pre:text-gray-800 dark:prose-pre:border-gray-700 dark:prose-pre:text-gray-200 dark:prose-pre:bg-zinc-900 rounded-lg"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
-    </article>
+    <main className="px-4 py-6 md:py-12 lg:py-16">
+      <article className="prose prose-zinc max-w-none mx-auto lg:max-w-6xl dark:prose-invert prose-pre:border prose-pre:bg-zinc-200 prose-pre:text-gray-800 dark:prose-pre:border-gray-700 dark:prose-pre:text-gray-200 dark:prose-pre:bg-zinc-900 rounded-lg">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          {post.title}
+        </h1>
+        <p className="text-gray-500 dark:text-gray-400">
+          Posted on{" "}
+          <time dateTime={post.date}>
+            {format(parseISO(post.date), "LLLL d, yyyy")}
+          </time>
+        </p>
+
+        <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      </article>
+    </main>
   )
 }
 
