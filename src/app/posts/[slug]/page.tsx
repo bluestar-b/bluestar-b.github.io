c7f5263e1f1a1354112153d40c9dacd0cb8bdd35
bluestar_ <canikissfemboy@gmail.com>
2024-02-11 14:34:51 +0700

    switch to mdx

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { format, parseISO } from "date-fns"
 import { allPosts } from "contentlayer/generated"
+import { useMDXComponent } from "next-contentlayer/hooks"
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }))
@@ -13,6 +14,7 @@ export const generateMetadata = ({ params }: { params: { slug: string } }) => {
 const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
+  const MDXContent = useMDXComponent(post.body.code)
 
   return (
     <main className="px-4 py-6 md:py-12 lg:py-16">
@@ -26,8 +28,7 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
             {format(parseISO(post.date), "LLLL d, yyyy")}
           </time>
         </p>
-
-        <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+        <MDXContent />
       </article>
     </main>
   )
