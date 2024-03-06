bf5ee0c641181428d2ed3ce6baa6258a9d9626a6
bluestar_ <canikissfemboy@gmail.com>
2024-03-06 11:45:31 +0700

    Remove unused import and fix tedt color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { compareDesc } from "date-fns"
 import { allPosts } from "contentlayer/generated"
 import { PostListLn } from "@/components/PostListLn"
 import Photo from "@/components/Photo"
-import Todo from "@/components/Todo"
 import TodoList from "@/components/TodoList"
+import ThemeToggler from "@/components/theme/ThemeToggler"
 
 export default function Home() {
   const posts = allPosts
@@ -15,13 +15,14 @@ export default function Home() {
   return (
     <main>
       <div className="flex-auto min-w-0 mt-6 flex flex-col p-3">
+        <ThemeToggler />
         <Heading />
         <div className="mb-2">
           I joined the world on April 3rd, 2008, and became a space enthusiast,
           studying in a non-formal education because life is crazy to me 🧨. I
           started learning things in 2020 and began applying them in 2021.
           <div className="mb-4 mt-1">
-            <div className="text-base font-light italic leading-relaxed text-gray-600 dark:text-gray-200">
+            <div className="text-base font-light italic leading-relaxed  ">
               &quot;Life is boring, but life still has colors to explore. Find a
               new color or make that color yourself.&quot;
             </div>
