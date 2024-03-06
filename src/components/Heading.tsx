fa19fc02e12d70f457aff1c9918010ac7b2983ec
bluestar_ <canikissfemboy@gmail.com>
2024-03-06 18:34:45 +0700

    Add more icon link

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,6 @@
+import { GithubLogo, InstagramLogo } from "@phosphor-icons/react/dist/ssr"
 import ThemeToggler from "./theme/ThemeToggler"
+import Link from "next/link"
 const Heading = () => {
   return (
     <div className="flex items-center justify-between py-4">
@@ -7,8 +9,20 @@ const Heading = () => {
         <div className="text-sm">I create bugs🤯</div>
       </div>
       <div className="flex items-center">
-        <div className="inline">
-          <div>
+        <div className="space-x-3">
+          <div className="inline-block">
+            <Link href="https://github.com/bluestar-b/" aria-label="Github">
+            <GithubLogo size={20} />
+            </Link>
+            
+          </div>
+          <div className="inline-block">
+            <Link href="https://instagram.com/bluestar.pics" aria-label="Instagram">
+            <InstagramLogo size={20} />
+            </Link>
+            
+          </div>
+          <div className="inline-block">
             <ThemeToggler />
           </div>
         </div>
