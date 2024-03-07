87cc42d2468ad6f6213c56b43e466ad61c819d13
bluestar_ <canikissfemboy@gmail.com>
2024-03-07 14:41:37 +0700

    reduce font size

--- src/components/Where.tsx
+++ src/components/Where.tsx
@@ -8,7 +8,7 @@ const redis = Redis.fromEnv()
 export default async function Where() {
   const location = await redis.get<string>("location")
   return (
-    <div className="flex text-sm font-medium items-center">
+    <div className="flex text-xs font-medium items-center">
       <Link href={`https://google.com/maps/search/${location}`}>
         <PaperPlaneTilt weight="bold" size={12} className="inline-block mr-1" />{" "}
         {location}
