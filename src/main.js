3b0ca7c90ac9c400c3b2d113648667712b336278
mac <flamekung123146@gmail.com>
2024-06-17 11:26:14 +0700

    Fix dracula css import

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import TimeAgo from "javascript-time-ago"
 
 import en from "javascript-time-ago/locale/en"
 
-import "./dracula.css"
-
 TimeAgo.addDefaultLocale(en)
 
 const timeAgo = new TimeAgo("en-US")
