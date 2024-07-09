23f11bab5280c9cfcc60755f779fcb0bc07d6664
mac <flamekung123146@gmail.com>
2024-07-10 01:09:18 +0700

    renovate

--- render.js
+++ render.js
@@ -1,28 +1,122 @@
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
-import { Marked } from "marked"
 import { JSDOM } from "jsdom"
+import markdownit from "markdown-it"
+import Shiki from "@shikijs/markdown-it"
 
-import { markedHighlight } from "marked-highlight"
-import hljs from "highlight.js"
+import { execSync } from "child_process"
 
 const contentsDir = path.resolve(process.cwd(), "contents")
-//Output Dir for contents
 const outputDir = path.resolve(process.cwd(), "blog")
-
 const metadataFile = path.resolve(process.cwd(), "metadata.json")
 let metadata = {}
 
-const marked = new Marked(
-  markedHighlight({
-    langPrefix: "hljs language-",
-    highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : "plaintext"
-      return hljs.highlight(code, { language }).value
-    },
+const md = new markdownit({
+  html: true,
+})
+md.use(
+  await Shiki({
+    theme: "andromeeda",
   }),
 )
+
+const commitID = execSync("git rev-parse --short HEAD", {
+  encoding: "utf8",
+}).trim()
+
+async function processFile(file) {
+  console.log("Processing", file)
+  const filePath = path.join(contentsDir, file)
+  const str = fs.readFileSync(filePath, "utf8")
+  const { content, data } = matter(str)
+
+  const baseFilename = path.basename(file, path.extname(file))
+  metadata[baseFilename] = data
+
+  const dom = new JSDOM(`
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>${data.title}</title>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap')
+    </style>
+      
+</head>
+
+<body class="prose md:prose-lg lg:prose-3xl p-2 prose-h1:text-3xl prose-invert prose-neutral mx-auto h-screen prose-img:rounded-xl prose-video:rounded-lg">
+<h2 class="text-center theme-toggle">${data.title}</h2>
+
+<div id="info" class="text-sm font-bold"></div>
+<div id="tags" class="not-prose items-center">
+<p class="inline-flex text-xs font-bold">Tags:</p>
+</div>
+</body>
+</html>
+`)
+
+  const { document } = dom.window
+
+  function setMetaTag(property, content) {
+    let metaTag = document.querySelector(`meta[property="${property}"]`)
+    if (!metaTag) {
+      metaTag = document.createElement("meta")
+      metaTag.setAttribute("property", property)
+      document.head.appendChild(metaTag)
+    }
+    metaTag.setAttribute("content", content)
+  }
+
+  function setHead(tagName, content) {
+    let tag = document.querySelector(tagName)
+    if (!tag) {
+      tag = document.createElement(tagName)
+      document.head.appendChild(tag)
+    }
+    tag.textContent = content
+  }
+
+  const container = document.getElementById("tags")
+  container.innerHTML += data.tags
+    .map(
+      (tag, index) =>
+        `<a class="inline-flex items-center text-sm font-bold text-[color:var(--link-text-color)]">${tag}${index < data.tags.length - 1 ? ", " : ""}</a>`,
+    )
+    .join("")
+
+  setMetaTag("og:title", data.title)
+  setMetaTag("og:type", "website")
+  setMetaTag("og:description", data.desc)
+
+  setHead("title", data.title)
+  setHead("meta", "charset=UTF-8")
+
+  document.body.innerHTML += md.render(content)
+
+  const scriptTag = document.createElement("script")
+  scriptTag.setAttribute("type", "module")
+  scriptTag.setAttribute("src", "/src/main.js")
+  document.body.appendChild(scriptTag)
+
+  /*
+  const footer = document.createElement("footer");
+  footer.className = "mt-auto text-white py-4";
+  footer.innerHTML = `<div class="mx-auto text-center text-xs">Generated on ${new Date().toLocaleString()}<br/></div>`;
+  document.body.appendChild(footer);
+*/
+  document.getElementById("info").innerHTML =
+    `Author: ${data.author}<br />Publish on: ${new Date(data.date).toLocaleString()}<br/>Commit ID: ${commitID}`
+
+  const htmlContent = dom.serialize()
+
+  const outputPath = path.join(outputDir, `${baseFilename}.html`)
+  fs.writeFileSync(outputPath, htmlContent)
+  console.log(`Converted ${file} to ${baseFilename}.html`)
+}
 
 fs.readdir(contentsDir, async (err, files) => {
   if (err) {
@@ -35,121 +129,15 @@ fs.readdir(contentsDir, async (err, files) => {
     console.log("No Markdown files found.")
     return
   }
+
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir)
   }
 
   for (const file of mdFiles) {
-    console.log("Saved metadata for", file)
-    const filePath = path.join(contentsDir, file)
-    const str = fs.readFileSync(filePath, "utf8")
-    const { data } = matter(str)
-
-    console.log(data.tags)
-    const baseFilename = path.basename(file, path.extname(file))
-    metadata[baseFilename] = data
+    await processFile(file)
   }
 
   fs.writeFileSync(metadataFile, JSON.stringify(metadata, null, 2))
   console.log(`Metadata saved to ${metadataFile}`)
-
-  for (const file of mdFiles) {
-    const filePath = path.join(contentsDir, file)
-    const str = fs.readFileSync(filePath, "utf8")
-    const { content, data } = matter(str)
-
-    const dom = new JSDOM(`
-<!DOCTYPE html>
-<html>
-
-<head>
-<title>${data.title}</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-</head>
-
-<body class="prose md:prose-lg lg:prose-xl p-4 prose-invert prose-neutral mx-auto h-screen prose-img:rounded-xl prose-video:rounded-lg">
-<h2 class="text-center">${data.title}</h1>
-<div id="info" class="text-xs"></div>
-
-
-<div id="tags" clsss="not-prose inline-flex">
-<p class="inline-flex text-xs font-medium">Tags:</p>
-
-</div>
-</body>
-</html>
-
-`)
-
-     // eslint-disable-next-line
-     const { document, window } = dom.window
-
-    function setMetaTag(property, content) {
-      let metaTag = document.querySelector(`meta[property="${property}"]\n`)
-      if (!metaTag) {
-        metaTag = document.createElement("meta")
-        metaTag.setAttribute("property", property)
-        document.head.appendChild(metaTag)
-      }
-      metaTag.setAttribute("content", content)
-      document.head.appendChild(document.createTextNode("\n"))
-    }
-
-    function setHead(tagName, content) {
-      let tag = document.querySelector(`${tagName}`)
-      if (!tag) {
-        tag = document.createElement(tagName)
-        document.head.appendChild(tag)
-      }
-      tag.textContent = content
-
-      document.head.appendChild(document.createTextNode("\n"))
-    }
-
-    const container = document.getElementById("tags")
-
-    container.innerHTML += data.tags
-      .map(
-        (tag, index) => `
-    <a class="inline-flex items-center text-sm text-[color:var(--link-text-color)]">${tag}${index < data.tags.length - 1 ? ", " : ""}</a>
-`,
-      )
-      .join("")
-
-    setMetaTag("og:title", data.title)
-    setMetaTag("og:type", "website")
-    setMetaTag("og:description", data.desc)
-
-    setHead("title", data.title)
-    setHead("meta", "charset=UTF-8")
-
-    document.body.innerHTML += marked.parse(content)
-
-    const scriptTag = document.createElement("script")
-    scriptTag.setAttribute("type", "module")
-    scriptTag.setAttribute("src", "/src/main.js")
-    document.body.appendChild(scriptTag)
-
-    const footer = document.createElement("footer")
-    footer.className = "mt-auto text-white py-4"
-    footer.innerHTML = `
-      <div class="mx-auto text-center text-xs">
-        Generated on ${new Date().toLocaleString()}<br/>
-      </div>
-    `
-    document.body.appendChild(footer)
-
-    document.getElementById("info").innerHTML =
-      `Author: ${data.author}<br />Publish on: ${data.date.toLocaleString()}`
-
-    const htmlContent = dom.serialize()
-
-    const baseFilename = path.basename(file, path.extname(file))
-    const filename = `${baseFilename}.html`
-    const outputPath = path.join(outputDir, filename)
-    fs.writeFileSync(outputPath, htmlContent)
-    console.log(`Converted ${file} to ${filename}`)
-  }
 })
--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import "./style.css"
 
-import TimeAgo from "javascript-time-ago"
-
-import en from "javascript-time-ago/locale/en"
-
-TimeAgo.addDefaultLocale(en)
-
-const timeAgo = new TimeAgo("en-US")
-
 import metadata from "/metadata.json"
 import Fuse from "fuse.js"
-
-
-import "./dracula.css"
 
 const list = Object.entries(metadata).map(([key, item]) => ({
   key,
@@ -53,50 +42,13 @@ function renderResults(results) {
   })
 }
 
-async function getCoords() {
-  const response = await fetch("https://ipapi.co/json/")
-  const data = await response.json()
-  return { latitude: data.latitude, longitude: data.longitude }
-}
-
-/*
-    Calculate the great circle distance between two points
-    on the Earth's surface given their latitude and longitude
-    in degrees.
-    
-    */
-function haversine(lat1, lon1, lat2, lon2) {
-  let dLat = ((lat2 - lat1) * Math.PI) / 180.0
-  let dLon = ((lon2 - lon1) * Math.PI) / 180.0
-
-  lat1 = (lat1 * Math.PI) / 180.0
-  lat2 = (lat2 * Math.PI) / 180.0
-
-  let a =
-    Math.pow(Math.sin(dLat / 2), 2) +
-    Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2)
-  let rad = 6371
-  let c = 2 * Math.asin(Math.sqrt(a))
-  return Math.ceil(rad * c)
-}
-
-getCoords().then((coords) => {
-  const lat2 = coords.latitude,
-    lon2 = coords.longitude
-  const lat1 = 14.771096,
-    lon1 = 100.691103
-  const distance = haversine(lat1, lon1, lat2, lon2)
-  document.getElementById("distance").innerHTML =
-    `Approximately ${distance.toFixed(2)} km away from you.🤯`
-})
-
 function updateHtml() {
   try {
     const articlesDiv = document.getElementById("articles")
     articlesDiv.innerHTML = ""
     for (const key in metadata) {
       const article = metadata[key]
-//      const tags = article.tags.map((tag) => `<li>${tag}</li>`).join("")
+      //      const tags = article.tags.map((tag) => `<li>${tag}</li>`).join("")
       const articleHtml = `
         <div>
           <a href="/blog/${key}">${article.title}</a>
@@ -124,110 +76,107 @@ searchBox.addEventListener("input", (e) => {
   }
 })
 
+async function getCoords() {
+  const response = await fetch("https://ipapi.co/json/")
+  const data = await response.json()
+  return { latitude: data.latitude, longitude: data.longitude }
+}
+
+/*
+    Calculate the great circle distance between two points
+    on the Earth's surface given their latitude and longitude
+    in degrees.
+
+*/
+function haversine(lat1, lon1, lat2, lon2) {
+  let dLat = ((lat2 - lat1) * Math.PI) / 180.0
+  let dLon = ((lon2 - lon1) * Math.PI) / 180.0
+
+  lat1 = (lat1 * Math.PI) / 180.0
+  lat2 = (lat2 * Math.PI) / 180.0
+
+  let a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2)
+  let rad = 6371
+  let c = 2 * Math.asin(Math.sqrt(a))
+  return Math.ceil(rad * c)
+}
+
+getCoords().then((coords) => {
+  const lat2 = coords.latitude,
+    lon2 = coords.longitude
+  const lat1 = 14.771096,
+    lon1 = 100.691103
+  const distance = haversine(lat1, lon1, lat2, lon2)
+  document.getElementById("distance").innerHTML =
+    `Approximately ${distance.toFixed(2)} km away from you.🤯`
+})
+
 const dropdownButton = document.getElementById("dropdown-button")
-const dropdownMenu = document.getElementById("dropdown-menu")
+let dropdownMenu
 let isDropdownOpen = false
+
+function createDropdownMenu() {
+  const menu = document.createElement("div")
+  menu.id = "dropdown-menu"
+  menu.classList.add(
+    "absolute",
+    "right-0",
+    "z-40",
+    "w-40",
+    "origin-top-right",
+    "rounded-md",
+    "bg-[#212529]",
+    "bg-[#424649]",
+    "font-medium",
+    "shadow-lg",
+    "ring-1",
+    "ring-[#D6D7DC]",
+    "ring-opacity-80",
+  )
+  menu.innerHTML = `
+    <div class="p-2 py-2" role="menu" aria-orientation="vertical" aria-labelledby="dropdown-button">
+      <a href="https://github.com/bluestar-b" class="mb-1 block rounded-md px-2 py-1 text-sm hover:bg-[#4d5154]" aria-label="github: bluestar-b" role="menuitem" target="_blank">Github</a>
+      <a href="https://discord.com/users/8291561798035046708" aria-label="discord: bluestar.meow" class="mb-1 block rounded-md px-2 py-1 text-sm hover:bg-[#4d5154]" role="menuitem" target="_blank">Discord</a>
+      <a href="https://www.instagram.com/bluestar.pics" aria-label="Instagram: bluestar.pics" class="mb-1 block rounded-md px-2 py-1 text-sm hover:bg-[#4d5154]" target="_blank" role="menuitem">Instagram</a>
+      <a href="mailto:canikissfemboy@gmail.com" class="mb-1 block rounded-md px-2 py-1 text-sm hover:bg-[#4d5154]" role="menuitem">Email</a>
+      <a onclick="document.body.remove();" class="mb-1 block rounded-md px-2 py-1 text-sm hover:bg-[#4d5154]" role="menuitem">Click me!!!</a>
+    </div>
+  `
+  return menu
+}
 
 function toggleDropdown() {
   isDropdownOpen = !isDropdownOpen
   if (isDropdownOpen) {
-    dropdownMenu.classList.remove("hidden", "fade-out")
-    dropdownMenu.classList.add("fade-in")
+    dropdownMenu = createDropdownMenu()
+    dropdownButton.parentElement.appendChild(dropdownMenu)
   } else {
-    dropdownMenu.classList.remove("fade-in")
-    dropdownMenu.classList.add("fade-out")
+    if (dropdownMenu) {
+      dropdownButton.parentElement.removeChild(dropdownMenu)
+      dropdownMenu = null
+    }
   }
 }
 
-dropdownButton.addEventListener("click", toggleDropdown)
+dropdownButton.addEventListener("click", (event) => {
+  event.stopPropagation() // Prevent the event from bubbling up to the window listener
+  toggleDropdown()
+})
 
 window.addEventListener("click", (event) => {
-  if (
-    !dropdownButton.contains(event.target) &&
-    !dropdownMenu.contains(event.target)
-  ) {
-    if (isDropdownOpen) {
-      dropdownMenu.classList.remove("fade-in")
-      dropdownMenu.classList.add("fade-out")
+  if (isDropdownOpen && dropdownMenu) {
+    if (
+      !dropdownButton.contains(event.target) &&
+      !dropdownMenu.contains(event.target)
+    ) {
+      dropdownButton.parentElement.removeChild(dropdownMenu)
+      dropdownMenu = null
+      isDropdownOpen = false
     }
   }
 })
-
-function TodoList() {
-  let visibleLabels = 5
-
-  const todos = [
-    { title: "Teacher's Day activity", date: "2023/06/25 00:00:00" },
-    {
-      title: "International Day Against Drug Abuse",
-      date: "2023/06/26 00:00:00",
-    },
-    {
-      title: "Scout Promise Review Ceremony",
-      date: "2023/07/01 00:00:00",
-    },
-    { title: "Study trip to Phetchaburi", date: "2023/07/22 00:00:00" },
-    {
-      title: "Candlelight procession, Wat Dong Noi, Lopburi",
-      date: "2023/07/27 00:00:00",
-    },
-    { title: "Scout camp", date: "2023/08/16 00:00:00" },
-    { title: "Scout camp(end)", date: "2023/08/18 00:00:00" },
-    {
-      title: "Field trip to Bang Saen And Marine Science Museum",
-      date: "2023/08/26 00:00:00",
-    },
-    { title: "Last activity (of the semester)", date: "2023/09/09 00:00:00" },
-    { title: "Exams #1/2023", date: "2023/09/16 00:00:00" },
-    { title: "Exams #2/2023", date: "2023/09/17 00:00:00" },
-    { title: "Semester break starts", date: "2023/09/18 00:00:00" },
-    { title: "Semester starts", date: "2023/11/05 00:00:00" },
-    {
-      title: "Teacher's Day and I got something!!",
-      date: "2024/01/15 00:00:00",
-    },
-    { title: "Academic camp", date: "2024/01/20 00:00:00" },
-    { title: "Field trip to Ayutthaya", date: "2024/02/20 00:00:00" },
-    { title: "Trip from Lopburi to Chonburi.", date: "2024/02/29 06:00:00" },
-    { title: "Exams #1/2024", date: "2024/03/02 06:00:00" },
-    { title: "Exams #2/2024", date: "2024/03/03 06:00:00" },
-    { title: "Semester starts", date: "2024/05/19 00:00:00" },
-    { title: "My birthday 2025", date: "2025/04/03 00:00:00" },
-  ].reverse()
-
-  const loadMore = () => {
-    visibleLabels += 5
-    render()
-  }
-
-  const render = () => {
-    const todoList = document.getElementById("todoList")
-    todoList.innerHTML = ""
-    todos.slice(0, visibleLabels).forEach((todo) => {
-      const div = document.createElement("div")
-      const span = document.createElement("span")
-      span.className = "text-md"
-      span.textContent = `${todo.title} - ${timeAgo.format(new Date(todo.date))}`
-      div.appendChild(span)
-      todoList.appendChild(div)
-    })
-
-    if (visibleLabels < todos.length) {
-      const button = document.createElement("button")
-      button.className = "text-blue-500"
-      button.textContent = "Load More"
-      button.addEventListener("click", loadMore)
-      todoList.appendChild(button)
-    }
-  }
-
-  render()
-
-  return null
-}
-
-const existingTodoListElement = document.getElementById("todoList")
-TodoList(existingTodoListElement)
 
 const images = [
   { src: "/images/sunset.jpg", alt: "Chasing the sun on two wheels." },
@@ -253,7 +202,6 @@ const images = [
 const randomIMG = images[Math.floor(Math.random() * images.length)]
 
 const imageElement = document.getElementById("randomImage")
-const photoInfoElement = document.getElementById("photoInfo")
 
 imageElement.src = randomIMG.src
 imageElement.alt = randomIMG.alt
@@ -261,4 +209,3 @@ imageElement.alt = randomIMG.alt
 const linkElement = document.createElement("a")
 linkElement.href = randomIMG.src
 linkElement.textContent = randomIMG.alt
-photoInfoElement.appendChild(linkElement)
