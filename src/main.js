e2ec58efbc22859b7b866a5393a8ebeba12a62ec
blue <canikissfemboy@gmail.com>
2025-03-19 18:54:55 +0700

    big update

--- src/main.js
+++ src/main.js
@@ -1,124 +1,31 @@
 import "./style.css"
+import Sortable from "sortablejs"
 
-/*
-import metadata from "/metadata.json"
-import Fuse from "fuse.js"
-
-const list = Object.entries(metadata).map(([key, item]) => ({
-  key,
-  ...item,
-  tags: Array.isArray(item.tags) ? item.tags.join(" ") : "",
-}))
-
-const options = {
-  includeScore: true,
-  keys: ["tags", "title", "author"],
-}
-
-const fuse = new Fuse(list, options)
-
-function renderResults(results) {
-  const resultsContainer = document.getElementById("articles")
-  resultsContainer.innerHTML = ""
-
-  if (results.length === 0) {
-    resultsContainer.innerHTML = "<p>No results found</p>"
-    return
-  }
-
-  results.sort((a, b) => new Date(b.item.date) - new Date(a.item.date))
-
-  const limitedResults = results.slice(0, 6)
-
-  limitedResults.forEach((result) => {
-    const item = result.item
-    const div = document.createElement("div")
-    div.classList.add("result-item")
-    div.innerHTML = `
-      <div>
-        <a class="blog-link" aria-label="${item.title}" href="/blog/${item.key}">${item.title}</a>
-      </div>
-    `
-    resultsContainer.appendChild(div)
-  })
-}
-
-function updateHtml() {
-  try {
-    const articlesDiv = document.getElementById("articles")
-    articlesDiv.innerHTML = ""
-    for (const key in metadata) {
-      const article = metadata[key]
-      //      const tags = article.tags.map((tag) => `<li>${tag}</li>`).join("")
-      const articleHtml = `
-        <div>
-          <a class="blog-link" aria-label=${article.title} href="/blog/${key}">${article.title}</a>
-        </div>
-      `
-      articlesDiv.insertAdjacentHTML("beforeend", articleHtml)
-    }
-
-    console.log("Articles updated in the browser.")
-  } catch (err) {
-    console.error("Error:", err)
-  }
-}
-
-updateHtml()
-
-const searchBox = document.getElementById("searchBox")
-searchBox.addEventListener("input", (e) => {
-  const query = e.target.value
-  if (query.trim() === "") {
-    updateHtml()
-  } else {
-    const results = fuse.search(query)
-    renderResults(results)
-  }
+// Puzzle grid
+const dragAndDropItems = document.getElementById("puzzle")
+new Sortable(dragAndDropItems, {
+  swapThreshold: 0.26,
+  invertSwap: true,
+  animation: 150,
 })
 
-*/
-/*
-async function getCoords() {
-  const response = await fetch("https://ipapi.co/json/")
-  const data = await response.json()
-  return { latitude: data.latitude, longitude: data.longitude }
-}
+const images = ["grid_0.jpg", "grid_1.jpg", "grid_2.jpg", "grid_3.jpg"]
 
-    Calculate the great circle distance between two points
-    on the Earth's surface given their latitude and longitude
-    in degrees.
+images.forEach((img) => {
+  const divElement = document.createElement("div")
+  divElement.classList.add("puzzle")
 
+  const imgElement = document.createElement("img")
+  imgElement.src = `/puzzlegrid/${img}`
+  imgElement.alt = `Image ${img}`
+  imgElement.classList.add("w-full", "h-full", "object-fill")
 
-function haversine(lat1, lon1, lat2, lon2) {
-  let dLat = ((lat2 - lat1) * Math.PI) / 180.0
-  let dLon = ((lon2 - lon1) * Math.PI) / 180.0
+  divElement.appendChild(imgElement)
 
-  lat1 = (lat1 * Math.PI) / 180.0
-  lat2 = (lat2 * Math.PI) / 180.0
-
-  let a =
-    Math.pow(Math.sin(dLat / 2), 2) +
-    Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2)
-  let rad = 6371
-  let c = 2 * Math.asin(Math.sqrt(a))
-  return Math.ceil(rad * c)
-}
-
-getCoords().then((coords) => {
-  const lat2 = coords.latitude,
-    lon2 = coords.longitude
-  const lat1 = 14.771096,
-    lon1 = 100.691103
-  const distance = haversine(lat1, lon1, lat2, lon2)
-  document.getElementById("distance").innerHTML =
-    `Approximately ${distance.toFixed(2)} km away from you. :3`
+  dragAndDropItems.appendChild(divElement)
 })
 
-
-
-*/
-
+//re-render image alt text
 document.querySelectorAll("#images img").forEach((img) => {
   const altText = img.getAttribute("alt")
 
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,5 @@
 import { defineConfig } from "vite"
-import { run } from "vite-plugin-run"
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
-
-import sitemapPlugin from "./sitemap"
-
-import fs from "fs"
-import path from "path"
-
-function getHtmlEntries(paths) {
-  const entries = {}
-
-  paths.forEach((pattern) => {
-    const baseDir = path.resolve(__dirname, pattern.baseDir)
-    const files = fs.readdirSync(baseDir)
-
-    files.forEach((file) => {
-      const filePath = path.join(baseDir, file)
-      const stat = fs.statSync(filePath)
-
-      if (!stat.isDirectory() && file.endsWith(".html")) {
-        const name = path.relative(__dirname, filePath).replace(/\\/g, "/")
-        entries[name] = filePath
-      }
-    })
-  })
-
-  return entries
-}
 
 const DEFAULT_OPTIONS = {
   test: /\.(jpe?g|png|webp)$/i,
@@ -47,28 +20,5 @@ const DEFAULT_OPTIONS = {
 }
 
 export default defineConfig({
-  plugins: [
-    sitemapPlugin({
-      baseUrl: "https://mac.notmycode.dev/",
-    }),
-
-    ViteImageOptimizer(DEFAULT_OPTIONS),
-
-    run([
-      {
-        name: "Render blog",
-        run: ["node", "render.js"],
-        pattern: ["src/**/*.js", "contents/**/*.md", "render.js"],
-      },
-    ]),
-  ],
-  build: {
-    rollupOptions: {
-      input: getHtmlEntries([
-        { baseDir: ".", patterns: ["blog.html"] },
-        { baseDir: ".", patterns: ["index.html"] },
-        { baseDir: "blog", patterns: ["*.html"] },
-      ]),
-    },
-  },
+  plugins: [ViteImageOptimizer(DEFAULT_OPTIONS)],
 })
