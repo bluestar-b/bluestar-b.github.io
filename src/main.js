64abce0f1a11aa5b222aecbbe8150db277e61512
bluestar <canikissfemboy@gmail.com>
2025-05-06 14:57:41 +0000

    fix: preload font for improved loading performance and add footer with image credit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+document.addEventListener("keydown", async function (e) {
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey &&
+      e.shiftKey &&
+      (e.key === "I" || e.key === "J" || e.key === "C")) ||
+    (e.ctrlKey && e.key === "U")
+  ) {
+    console.log(
+      "%cmeow <3 stop peeking at my ass",
+      "color: hotpink; font-size: 20px; font-weight: bold; background: black; padding: 5px; border-radius: 5px;",
+    );
+  }
+});
